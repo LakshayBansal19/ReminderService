154b861bf3ac4dbdfc50f4a2a42de6ce6028cccd
Lakshay Bansal <lakshaybansal1905@gmail.com>
2025-05-17 22:38:05 +0530

    subscribeEvent function added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const{PORT}=require('./config/serverConfig');
 
+const{createChannel, subscribeMessage}=require('./utils/messageQueue');
+const{REMINDER_BINDING_KEY}=require('./config/serverConfig');
+
 const {sendBasicEmail}=require('./services/email-service');
 const jobs=require('./utils/job');
 const TicketController=require('./controllers/ticket-controller');
+
+const EmailService=require('./services/email-service'); 
+
 const startAndSetUpServer=async()=>{
 
     const app=express();
@@ -14,10 +20,13 @@ const startAndSetUpServer=async()=>{
 
     app.use(bodyParser.urlencoded({extended:true}));
 
+    const channel=await createChannel();
+    subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY);
+    
     app.post('/api/v1/tickets',TicketController.create);
     app.listen(PORT,()=>{
         console.log(`Server started at Port ${PORT}`);
-        jobs();
+        //jobs();
 
         // sendBasicEmail(
         //     'support@admin.com',
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -45,9 +45,25 @@ const updateTicket=async(ticketID,status)=>{
         console.log(error);
     }
 }
+const subscribeEvents = async (payload)=>{
+    let service=payload.service;
+    let data=payload.data;
+    switch(service){
+        case 'CREATE_TICKET':
+            await createNotification(data);
+            break;
+        case 'SEND_BASIC_EMAIL':
+            await sendBasicEmail(data);
+            break;
+        default:
+            console.log('No valid event received');
+            break; 
+    }
+}
 module.exports={
     sendBasicEmail,
     fetchPendingEmails,
     createNotification,
-    updateTicket
+    updateTicket,
+    subscribeEvents
 }
